b493eeb83c1b3ae9e17ff602a93b68516a2feac9
rohit <rohitsb96134@gmail.com>
2025-01-17 16:05:08 +0530

    add dotenv and mongoose for environment configuration and database connection

--- api/index.js
+++ api/index.js
@@ -2,6 +2,16 @@ const express = require("express");
 const router = express.Router();
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
+const  dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_KEY).then(() => {
+    console.log("Connected to database!");
+  }).catch(() => {
+    console.log("Connection failed!");
+  });
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
