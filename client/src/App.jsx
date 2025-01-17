7fd77126bb48c941db57d16244a921ff14efd5b1
rohit <rohitsb96134@gmail.com>
2025-01-17 15:43:52 +0530

    implement routing with react-router-dom and create basic page components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,26 @@
-
+import { BrowserRouter, Routes ,Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Profile from "./pages/Profile";
+import Signout from "./pages/Signout";
+import Header from "./components/Header";
 
 function App() {
  
 
   return (
-    <>
-   <h1>hello</h1>
-    </>
-  )
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signout" element={<Signout />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
