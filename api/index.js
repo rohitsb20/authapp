ab1ea1394c0749dff96241b90269a6b49e0c33d3
rohit <rohitsb96134@gmail.com>
2025-01-17 15:50:52 +0530

    add a basic route to respond with "Hello World!" on the root path

--- api/index.js
+++ api/index.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+    });
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
