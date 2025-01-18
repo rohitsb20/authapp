836433a6ef03ba368e751faa0ddc2cfd1df2f257
rohit <rohitsb96134@gmail.com>
2025-01-18 12:21:07 +0530

    signu api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,3 +1,23 @@
-export const signup = (req, res) => {
-  console.log(req.body);
+import User from "../model/user.model.js";
+import bcrypt from "bcryptjs";
+export const signup = async (req, res) => {
+
+  const {name, email, password} = req.body;
+
+  const hashPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({name, email, password: hashPassword});
+  
+
+
+  try {
+    await newUser.save();
+    res.status(201).json({
+      message: "User created successfully",
+    });
+  } catch (error) {
+    res.status(409).json({
+      message: "User already exists",
+    });
+  }
+   
 };
