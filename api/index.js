4d4c1421feff2fe7d708adc22b562c0fa5aa3fe1
rohit <rohitsb96134@gmail.com>
2025-01-18 11:34:24 +0530

    refactor: remove unused user controller and routes; streamline user model definition

--- api/index.js
+++ api/index.js
@@ -1,24 +1,21 @@
-import express from "express";
-import userRoutes from "./routes/user.route.js";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 dotenv.config();
 
+mongoose.connect(process.env.MONGO_KEY)
+.then(() => { console.log('Connected to database')})
+.catch(() => {console.log("Connected failed to database")});
+
+
+
+
+
+
+
+
+
+
 const app = express();
-const port = 3000;
-
-mongoose
-  .connect(process.env.MONGO_KEY)
-  .then(() => {
-    console.log("Connected to database!");
-  })
-  .catch(() => {
-    console.log("Connection failed!");
-  });
-
-app.use("/api/users", userRoutes);
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(3000, () => {console.log('Server is running on port 3000');
+})
